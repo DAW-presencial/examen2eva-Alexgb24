3308c98f66722fe3db196840c13a36ca9f3974c3
Alexgb24 <agonzalez@cifpfbmoll.eu>
2021-02-24 20:01:43 +0100

    tarea 6, tratar errores con blade

--- resources/views/tutor.blade.php
+++ resources/views/tutor.blade.php
@@ -15,26 +15,53 @@
                 <div class="mb-3 mt-5">
                     <h2>Tutor</h2>
                 </div>
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        {{ $error }}
+                    </div>
+                @endif
+                
                 <form action="">
                     <label for="empresa" class="form-label">{{ __("empresa") }}</label>
                     <input required type="text" id="empresa" name="empresa" class="form-control" value="{{ old('empresa') }}"/>
-
+                    @error('empresa')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="tipo_documento" class="form-label">{{ __("tipo_documento") }}</label>
                     <select  required class="form-select form-select-sm" id="tipo_documento" name="tipo_documento">
                         <option value="nif" {{ old("tipo_documento") == "nif" ? "selected" : "" }}>{{ __("tutores.nif") }}</option>
                         <option value="nie" {{ old("tipo_documento") == "nie" ? "selected" : "" }}>{{ __("tutores.nie") }}</option>
                         <option value="pasaporte" {{ old("tipo_documento") == "pasaporte" ? "selected" : "" }}>{{ __("tutores.pasaporte") }}</option>
                     </select>
-
+                    @error('tipo_documento')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="dni" class="form-label">{{ __("dni") }}</label>
                     <input required type="text" id="dni" name="dni" class="form-control" value="{{ old('dni') }}"/>
-
+                    @error('dni')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="nombre" class="form-label">{{ __("nombre") }}</label>
                     <input required type="text" id="nombre" name="nombre" class="form-control" value="{{ old('nombre') }}"/>
-
+                    @error('nombre')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="primer_apellido" class="form-label">{{ __("primer_apellido") }}</label>
                     <input required type="text" id="primer_apellido" name="primer_apellido" class="form-control" value="{{ old('primer_apellido') }}"/>
-
+                    @error('primer_apellido')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="segundo_apellido" class="form-label">{{ __("segundo_apellido") }}</label>
                     <input type="text" id="segundo_apellido" name="segundo_apellido" class="form-control" value="{{ old('segundo_apellido') }}"/>
 
@@ -52,13 +79,25 @@
                         <option value="activo" {{ old("estado") == "activo" ? "selected" : "" }}>{{ __("tutores.activo") }}</option>
                         <option value="inactivo" {{ old("estado") == "inactivo" ? "selected" : "" }}>{{ __("tutores.inactivo") }}</option>
                     </select>
-
+                    @error('estado')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="telefono" class="form-label">{{ __("telefono") }}</label>
-                    <input  required type="text" id="telefono" name="telefono" class="form-control" value="{{ old('telefono') }}"/>
-
+                    <input required type="text" id="telefono" name="telefono" class="form-control" value="{{ old('telefono') }}"/>
+                    @error('telefono')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <label for="email" class="form-label">Mail</label>
                     <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}"/>
-                    
+                    @error('email')
+                        <div class="alert alert-danger mb-2" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <button type="submit" class="btn btn-secondary">{{ __("tutores.enviar") }}</button>
                 </form>
                 
